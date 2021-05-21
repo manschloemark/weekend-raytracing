52feae84a6a90e83ff824bd1650416221bea4caa
Mark Schloeman <markofwisdumb@gmail.com>
2021-05-21 14:39:18 -0400

    Working on chapter 7. Bugged atm!

--- common.h
+++ common.h
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <cstdlib>
 
 // Using
 using std::shared_ptr;
@@ -18,6 +19,25 @@ const double pi = 3.1415926535897932385;
 inline double degrees_to_radians(double degrees)
 {
 	return degrees * pi / 180.0;
+}
+
+inline double random_double()
+{
+	// returns a double in range [0, 1)
+	return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max)
+{
+	// returns a double in range [min, max)
+	return min + (max - min) * random_double();
+}
+
+inline double clamp(double x, double min, double max)
+{
+	if ( x < min ) return min;
+	if ( x > max ) return max;
+	return x;
 }
 
 //Common Headers
