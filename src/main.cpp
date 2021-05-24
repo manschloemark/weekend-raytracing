811ee2d8b320b57641d499c00fa9211dd74b8c03
Mark Schloeman <markofwisdumb@gmail.com>
2021-05-24 15:14:05 -0400

    Finished book one.

--- src/main.cpp
+++ src/main.cpp
@@ -30,31 +30,105 @@ color ray_color(const ray& r, const hittable& world, int depth)
 	return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
+hittable_list random_scene() {
+	hittable_list world;
+
+	auto mat_ground = make_shared<lambertian>(color(0.4, 0.4, 0.2));
+
+	world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, mat_ground));
+
+	for (int a = -11; a < 11; a++)
+	{
+	  for (int b = -11; b < 11; b++)
+	  {
+	    double radius = 0.4 * random_double(0.9, 0.99);
+	    point3 center = point3(a + 0.9 * random_double(), radius, b + 0.9 * random_double());
+
+	    if (random_double() > 0.66) continue;
+
+	    double material = random_double();
+
+	    if (material <= 0.33) // Diffuse (lambertian)
+	    {
+	      auto albedo = color::random() * color::random();
+	      auto mat = make_shared<lambertian>(albedo);
+	      auto random_sphere = make_shared<sphere>(center, radius, mat);
+	      world.add(random_sphere);
+      }
+	    else if (material <= 0.66) // Reflective (metal)
+	    {
+	      auto albedo = color::random();
+	      auto fuzziness = random_double();
+	      auto mat = make_shared<metal>(albedo, fuzziness);
+	      auto random_sphere = make_shared<sphere>(center, radius, mat);
+				world.add(random_sphere);
+      }
+	    else // Refractive (dialectric)
+	    {
+	      auto incidence_of_refraction = random_double(0.5, 1.9);
+	      auto mat = make_shared<dialectric>(incidence_of_refraction);
+	      auto random_sphere = make_shared<sphere>(center, radius, mat);
+	      world.add(random_sphere);
+	    }
+    }
+  }
+	auto mat_one = make_shared<dialectric>(1.5);
+	auto mat_two = make_shared<metal>(color(0.6, 0.7, 0.8), 0.0);
+	auto mat_three = make_shared<lambertian>(color(0.25, 0.8, 0.55));
+
+	world.add(make_shared<sphere>(point3(-2, 1, 0), 1.0, mat_one));
+	world.add(make_shared<sphere>(point3(2, 1, -2), 1.0, mat_two));
+	world.add(make_shared<sphere>(point3(-5, 1, -4), 1.0, mat_three));
+
+	return world;
+}
+
 int main()
 {
 	// Image
 
-	const auto aspect_ratio = 16.0 / 9.0;
-	const int image_width = 400;
+	const auto aspect_ratio = 3.0 / 2.0;
+	const int image_width = 1200;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
-	const int samples_per_pixel = 100;
-	const int max_depth = 50;
+	const int samples_per_pixel = 50;
+	const int max_depth = 40;
 
 	// World / Scene
-  hittable_list world;
-
+  auto world = random_scene();
+	// Scene from book
+  /*
   auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
   auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
-  auto material_left = make_shared<metal>(color(0.8, 0.8, 0.8), 0.4);
-  auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
+  auto material_left = make_shared<dialectric>(1.5);
+  auto material_right = make_shared<metal>(color(0.8, 0.8, 0.8), 1.0);
 
   world.add(make_shared<sphere>(point3(0.0, -100.5, -1), 100, material_ground));
   world.add(make_shared<sphere>(point3(-1.0, 0.0, -1), 0.5, material_left));
+  world.add(make_shared<sphere>(point3(-1.0, 0.0, -1), -0.45, material_left));
   world.add(make_shared<sphere>(point3(0.0, 0.0, -1), 0.5, material_center));
   world.add(make_shared<sphere>(point3(1.0, 0.0, -1), 0.5, material_right));
 
+  auto charcoal = make_shared<lambertian>(color(0.2, 0.2, 0.2));
+	auto stainless_steel = make_shared<metal>(color(0.7, 0.7, 0.7), 1.0);
+	auto copper = make_shared<metal>(color(0.8, 0.55, 0.2), 0.55);
+	auto glass = make_shared<dialectric>(1.5);
+	auto earth = make_shared<lambertian>(color(0.3, 0.8, 0.1));
+
+	world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.4, glass));
+	world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.4, stainless_steel));
+	world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.4, copper));
+	world.add(make_shared<sphere>(point3(0.0, 1.0, -1.0), 0.4, charcoal));
+	world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, earth));
+*/
+
 	// Camera
-  camera cam;
+  point3 lookfrom(12, 2, 1);
+  point3 lookat(0, 0, -2);
+  vec3 vup(0, 1, 0);
+  double vfov = 90;
+  auto dist_to_focus = 10.0;
+  auto aperture = 0.1;
+  camera cam(lookfrom, lookat, vup, vfov, aspect_ratio, aperture, dist_to_focus);
 
 	std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
--- src/material.h
+++ src/material.h
@@ -52,4 +52,52 @@ class metal : public material {
 		double fuzz;
 };
 
+/*
+   I don't totally understand the math behind scatter here, but I would like to.
+   I know it uses Snell's law, which is
+   n * sin(theta) = n' * sin(theta')
+   Where n is the index of refraction of the medium the ray is traveling through
+   and n' is the index of refraction it is entering.
+   theta is the angle the ray hits the point where it changes material.
+   We use this formula to determine the angle the ray will travel at after the hit.
+*/
+class dialectric : public material {
+	public:
+		dialectric(double index_of_refraction) : ir(index_of_refraction) {}
+
+		virtual bool scatter(
+				const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
+			) const override {
+			attenuation = color(1.0, 1.0, 1.0); // Always 1 since glass absorbs no light.
+
+			double refraction_ratio = rec.front_face ? (1.0 / ir) : ir;
+			vec3 unit_direction = unit_vector(r_in.direction());
+
+			double cos_theta = fmin(dot(-unit_direction, rec.normal), 1.0);
+			double sin_theta = sqrt(1.0 - cos_theta * cos_theta);
+
+			bool cannot_refract = (refraction_ratio * sin_theta > 1.0);
+			vec3 direction;
+			if (cannot_refract || reflectance(cos_theta, refraction_ratio) > random_double()) {
+				// Must reflect the ray when refraction ratio * angle is greater than 1
+				direction = reflect(unit_direction, rec.normal);
+			} else {
+				direction = refract(unit_direction, rec.normal, refraction_ratio);
+			}
+			scattered = ray(rec.p, direction);
+			return true;
+		}
+		
+	public:
+		double ir;
+
+	private:
+		static double reflectance(double cosine, double ref_idx) {
+			// Shlick's approximation
+      auto r0 = (1 - ref_idx) / (1 + ref_idx);
+      r0 = r0 * r0;
+      return r0 + (1 - r0) * pow((1 - cosine), 5);
+		}
+};
+
 #endif
