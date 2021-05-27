e885f219230795592f95f83eaa84b2acf959b7e0
Mark Schloeman <markofwisdumb@gmail.com>
2021-05-26 20:42:10 -0400

    Made scenes.h which has functions that return scenes (hittable_lists)

---
+++ src/scenes.h
@@ -0,0 +1,95 @@
+#ifndef SCENES_H
+#define SCENES_H
+
+#include "common.h"
+#include "hittable_list.h"
+#include "material.h"
+#include "sphere.h"
+#include "moving_sphere.h"
+
+hittable_list random_scene() {
+	hittable_list world;
+
+	auto mat_ground = make_shared<lambertian>(color(0.4, 0.4, 0.2));
+
+	world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, mat_ground));
+
+	for (int a = -11; a < 11; a++)
+	{
+	  for (int b = -11; b < 11; b++)
+	  {
+	    double radius = 0.4 * random_double(0.9, 0.99);
+	    point3 center = point3(a + 0.9 * random_double(), radius, b + 0.9 * random_double());
+
+	    if (random_double() > 0.66) continue;
+
+	    double material = random_double();
+
+	    if (material <= 0.7) // Diffuse (lambertian)
+	    {
+	      auto albedo = color::random() * color::random();
+	      auto mat = make_shared<lambertian>(albedo);
+	      auto center2 = center + vec3(0, random_double(0, 0.5), 0);
+	      auto random_sphere = make_shared<moving_sphere>(center, center2, 0.0, 1.0, radius, mat);
+	      world.add(random_sphere);
+      }
+	    else if (material <= 0.85) // Reflective (metal)
+	    {
+	      auto albedo = color::random();
+	      auto fuzziness = random_double();
+	      auto mat = make_shared<metal>(albedo, fuzziness);
+	      auto random_sphere = make_shared<sphere>(center, radius, mat);
+				world.add(random_sphere);
+      }
+	    else // Refractive (dialectric)
+	    {
+	      auto incidence_of_refraction = random_double(0.5, 1.9);
+	      auto mat = make_shared<dialectric>(incidence_of_refraction);
+	      auto random_sphere = make_shared<sphere>(center, radius, mat);
+	      world.add(random_sphere);
+	    }
+    }
+  }
+	auto mat_one = make_shared<dialectric>(1.5);
+	auto mat_two = make_shared<metal>(color(0.6, 0.7, 0.8), 0.0);
+	auto mat_three = make_shared<lambertian>(color(0.25, 0.8, 0.55));
+
+	world.add(make_shared<sphere>(point3(-2, 1, 0), 1.0, mat_one));
+	world.add(make_shared<sphere>(point3(2, 1, -2), 1.0, mat_two));
+	world.add(make_shared<sphere>(point3(-5, 1, -4), 1.0, mat_three));
+
+	return world;
+}
+
+hittable_list simple_demo_scene()
+{
+
+	// World / Scene
+  auto world = random_scene();
+	// Scene from book
+  auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+  auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
+  auto material_left = make_shared<dialectric>(1.5);
+  auto material_right = make_shared<metal>(color(0.8, 0.8, 0.8), 1.0);
+
+  world.add(make_shared<sphere>(point3(0.0, -100.5, -1), 100, material_ground));
+  world.add(make_shared<sphere>(point3(-1.0, 0.0, -1), 0.5, material_left));
+  world.add(make_shared<sphere>(point3(-1.0, 0.0, -1), -0.45, material_left));
+  world.add(make_shared<sphere>(point3(0.0, 0.0, -1), 0.5, material_center));
+  world.add(make_shared<sphere>(point3(1.0, 0.0, -1), 0.5, material_right));
+
+  auto charcoal = make_shared<lambertian>(color(0.2, 0.2, 0.2));
+	auto stainless_steel = make_shared<metal>(color(0.7, 0.7, 0.7), 1.0);
+	auto copper = make_shared<metal>(color(0.8, 0.55, 0.2), 0.55);
+	auto glass = make_shared<dialectric>(1.5);
+	auto earth = make_shared<lambertian>(color(0.3, 0.8, 0.1));
+
+	world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.4, glass));
+	world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.4, stainless_steel));
+	world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.4, copper));
+	world.add(make_shared<sphere>(point3(0.0, 1.0, -1.0), 0.4, charcoal));
+	world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, earth));
+
+	return world;
+}
+#endif
