757feb7ba100687aea4da7c6cd04b0033a8f7797
Mark Schloeman <markofwisdumb@gmail.com>
2021-05-30 18:03:21 -0400

    End of chapter 8. Added rects, boxes, translation and rotation.

--- src/main.cpp
+++ src/main.cpp
@@ -13,26 +13,27 @@
 
 #include <iostream>
 
-color ray_color(const ray& r, const hittable& world, int depth)
+color ray_color(const ray& r, const color& background, const hittable& world, int depth)
 {
 	hit_record rec;
+
 	if (depth <= 0)
 	{
-	  return color(0, 0, 0);
-  }
+		return color(0, 0, 0);
+	}
 
-	if (world.hit(r, 0.001, infinity, rec))
-	{
-	  ray scattered;
-	  color attenuation;
-	  if (rec.mat_ptr->scatter(r, rec, attenuation, scattered))
-		  return attenuation * ray_color(scattered, world, depth - 1);
-	  return color(0, 0, 0);
-  }
+	if (!world.hit(r, 0.001, infinity, rec))
+		return background;
 
-	vec3 unit_direction = unit_vector(r.direction());
-	auto t = 0.5 * (unit_direction.y() + 1.0);
-	return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+	ray scattered;
+	color attenuation;
+
+	color emitted = rec.mat_ptr->emitted(rec.u, rec.v, rec.p);
+
+	if (!rec.mat_ptr->scatter(r, rec, attenuation, scattered))
+		return emitted;
+
+	return emitted + attenuation * ray_color(scattered, background, world, depth - 1);
 }
 
 
@@ -40,10 +41,10 @@ int main()
 {
 	// Image
 
-	const auto aspect_ratio = 16.0 / 9.0;
-	const int image_width = 800;
-	const int samples_per_pixel = 100;
-	const int max_depth = 50;
+	auto aspect_ratio = 16.0 / 9.0;
+	int image_width = 720;
+	int samples_per_pixel = 100;
+	int max_depth = 50;
 
 	// World / Scene / Camera
 	hittable_list world;
@@ -51,6 +52,9 @@ int main()
 	point3 lookat;
 	auto vfov = 40;
 	auto aperture = 0.0;
+
+	color background(0, 0, 0);
+
 	vec3 vup(0, 1, 0);
 	auto dist_to_focus = 10.0;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
@@ -58,25 +62,28 @@ int main()
 	timer t;
 	t.start();
 	// Switch statement to pick different scene setups.
-	switch(7) {
+	switch(12) {
 		case 1:
 			world = random_scene();
 			lookfrom = point3(13, 5, 3);
 			lookat = point3(0, 0, 0);
 			vfov = 30;
 			aperture = 0.1;
+			background = color(0.7, 0.8, 1.0);
 			break;
 		case 2:
 			world = two_spheres();
 			lookfrom = point3(13, 2, 3);
 			lookat = point3(0, 0, 0);
 			vfov = 20.0;
+			background = color(0.7, 0.8, 1.0);
 			break;
 		case 3:
 			world = two_perlin_spheres();
 			lookfrom = point3(13, 3, 3);
 			lookat = point3(0, 0, 0);
 			vfov = 40.0;
+			background = color(0.7, 0.8, 1.0);
 			break;
 		case 4:
 			world = my_textures();
@@ -85,6 +92,7 @@ int main()
 			vfov = 60;
 			aperture = 0.0;
 			dist_to_focus = 2.0;
+			background = color(0.7, 0.8, 1.0);
 			break;
 		case 5:
 			world = view_texture(make_shared<noise_texture>());
@@ -93,18 +101,69 @@ int main()
 			vfov = 90;
 			aperture = 0.0;
 			dist_to_focus = 1.0;
+			background = color(0.7, 0.8, 1.0);
 			break;
 		case 6:
 			world = two_turbulent_perlin_spheres();
 			lookfrom = point3(13, 3, 3);
 			lookat = point3(0, 0, 0);
 			vfov = 40.0;
+			background = color(0.7, 0.8, 1.0);
 			break;
 		case 7:
 			world = earth();
 			lookfrom = point3(0, 0, 0);
 			lookat = point3(0, 0, -1);
 			vfov = 30;
+			background = color(0.7, 0.8, 1.0);
+			break;
+		case 8:
+			world = simple_light();
+			samples_per_pixel = 200;
+			background = color(0, 0, 0);
+			lookfrom = point3(26, 3, 6);
+			lookat = point3(0, 2, 0);
+			vfov = 30.0;
+			break;
+		case 9:
+			world = solar_system();
+			samples_per_pixel = 600;
+			background = color(0, 0, 0);
+			lookfrom = point3(0, 5, 5);
+			lookat = point3(0, 0, 0);
+			vfov = 90;
+			break;
+		case 10:
+		// @TODO finish this.
+			world = playground();
+			samples_per_pixel = 100;
+			background = color(0, 0, 0);
+			lookfrom = point3(0, 12, 5);
+			lookat = point3(0, 0, 0);
+			vfov = 60;
+			aperture = 0.0;
+			dist_to_focus = (lookat - lookfrom).length();
+			break;
+		case 11:
+			world = light_test();
+			samples_per_pixel = 200;
+			background = color(0, 0, 0);
+			lookfrom = point3(0, 10, 0);
+			lookat = point3(0, 0, 0);
+			vup = vec3(0, 0, -1);
+			dist_to_focus = (lookat - lookfrom).length();
+			aperture = 0.0;
+			vfov = 30;
+			break;
+		case 12:
+			world = cornell_box();
+			samples_per_pixel = 100;
+			background = color(0, 0, 0);
+			aspect_ratio = 1.0;
+			image_width = 600;
+			lookfrom = point3(278, 278, -800);
+			lookat = point3(278, 278, 0);
+			vfov = 40.0;
 			break;
 		default:
 			world = material_demo_scene();
@@ -136,7 +195,7 @@ int main()
 				auto u = double(i + random_double()) / (image_width - 1);
 				auto v = double(j + random_double()) / (image_height - 1);
 				ray r = cam.get_ray(u, v);
-				pixel_color += ray_color(r, bvh, max_depth);
+				pixel_color += ray_color(r, background, bvh, max_depth);
 			}
 			write_color(std::cout, pixel_color, samples_per_pixel);
 		}
--- src/scenes.h
+++ src/scenes.h
@@ -6,6 +6,8 @@
 #include "material.h"
 #include "sphere.h"
 #include "moving_sphere.h"
+#include "aarect.h"
+#include "box.h"
 
 hittable_list random_scene() {
 	hittable_list world;
@@ -133,6 +135,34 @@ hittable_list earth() {
 	return hittable_list(globe);
 }
 
+hittable_list cornell_box() {
+    hittable_list objects;
+
+    auto red   = make_shared<lambertian>(color(.65, .05, .05));
+    auto white = make_shared<lambertian>(color(.73, .73, .73));
+    auto green = make_shared<lambertian>(color(.12, .45, .15));
+    auto light = make_shared<diffuse_light>(color(15, 15, 15));
+
+    objects.add(make_shared<yz_rect>(0, 555, 0, 555, 555, green));
+    objects.add(make_shared<yz_rect>(0, 555, 0, 555, 0, red));
+    objects.add(make_shared<xz_rect>(213, 343, 227, 332, 554, light));
+    objects.add(make_shared<xz_rect>(0, 555, 0, 555, 0, white));
+    objects.add(make_shared<xz_rect>(0, 555, 0, 555, 555, white));
+    objects.add(make_shared<xy_rect>(0, 555, 0, 555, 555, white));
+
+	shared_ptr<hittable> box1 = make_shared<box>(point3(0, 0, 0), point3(165, 330, 165), white);
+	box1 = make_shared<rotate_y>(box1, 15);
+	box1 = make_shared<translate>(box1, vec3(265, 0, 295));
+	objects.add(box1);
+
+	shared_ptr<hittable> box2 = make_shared<box>(point3(0, 0, 0), point3(165, 165, 165), white);
+	box2 = make_shared<rotate_y>(box2, -18);
+	box2 = make_shared<translate>(box2, vec3(130, 0, 65));
+	objects.add(box2);
+
+	return objects;
+}
+
 hittable_list my_textures() {
 	hittable_list objects;
 
@@ -153,7 +183,102 @@ hittable_list view_texture(shared_ptr<texture> t) {
 	objects.add(make_shared<sphere>(point3(0, 0, -1011), 1000.5, make_shared<lambertian>(t)));
 
 	return objects;
+}
 
+hittable_list simple_light() {
+	hittable_list objects;
 
+	auto pertext = make_shared<marbled_noise_texture>(4);
+	objects.add(make_shared<sphere>(point3(0, -1000, 0), 1000, make_shared<lambertian>(pertext)));
+	objects.add(make_shared<sphere>(point3(0, 2, 0), 2, make_shared<lambertian>(pertext)));
+
+	auto difflight = make_shared<diffuse_light>(color(4, 4, 4));
+	objects.add(make_shared<xy_rect>(3, 5, 2, 4, -2, difflight));
+	//objects.add(make_shared<sphere>(point3(3, 4, -2), 1, difflight));
+
+	return objects;
+}
+
+hittable_list solar_system() {
+	hittable_list objects;
+
+	auto pertext = make_shared<marbled_noise_texture>(4);
+	auto difflight = make_shared<diffuse_light>(color(5, 4.5, 4));
+	auto mars_texture = make_shared<marbled_noise_texture>(3);
+	auto earth_texture = make_shared<image_texture>("../resources/earth.jpg");
+	auto earth_surface = make_shared<lambertian>(earth_texture);
+
+	auto sun = make_shared<sphere>(point3(0, 0, 0), 2, difflight);
+	auto mars = make_shared<sphere>(point3(2, -1.5, 0), 0.7, make_shared<lambertian>(mars_texture));
+	auto terra = make_shared<sphere>(point3(4, 0, 0), 1, earth_surface);
+	auto moon = make_shared<sphere>(point3(4.2, 1.4, 1.2), 0.2, make_shared<lambertian>(pertext));
+
+	objects.add(sun);
+	objects.add(mars);
+	objects.add(terra);
+	objects.add(moon);
+
+	return objects;
+}
+
+hittable_list light_test() {
+	hittable_list objects;
+
+	double sphere_size = 0.35;
+
+	auto light_matr = make_shared<diffuse_light>(color(3, 1, 1));
+	auto lightr = make_shared<sphere>(point3(0, 0, 1), sphere_size, light_matr);
+	auto light_matg = make_shared<diffuse_light>(color(1, 3, 1));
+	auto lightg = make_shared<sphere>(point3(0.866, 0, -0.499), sphere_size, light_matg);
+	auto light_matb = make_shared<diffuse_light>(color(1, 1, 3));
+	auto lightb = make_shared<sphere>(point3(-0.866, 0, -0.499), sphere_size, light_matb);
+
+	auto mat1 = make_shared<metal>(color(0.8, 0.8, 0.8), 0.0);
+	auto ball1 = make_shared<sphere>(point3(0, 0, -1), sphere_size, mat1);
+	auto mat2 = make_shared<metal>(color(0.8, 0.8, 0.8), 0.5);
+	auto ball2 = make_shared<sphere>(point3(0.866, 0, 0.499), sphere_size, mat2);
+	auto mat3 = make_shared<metal>(color(0.8, 0.8, 0.8), 1.0);
+	auto ball3 = make_shared<sphere>(point3(0.866, 0, 0.499), sphere_size, mat3);
+
+	auto glass_mat = make_shared<dialectric>(1.5);
+	auto glass_ball = make_shared<sphere>(point3(0, 0, 0), sphere_size, glass_mat);
+	//auto glass_ball2 = make_shared<sphere>(point3(0, 3, 0), sphere_size, glass_mat);
+
+	auto bg_reflection = make_shared<metal>(color(1.0, 1.0, 1.0), 0.0);
+	objects.add(make_shared<sphere>(point3(-4, -1000, -10), 999, bg_reflection));
+
+	objects.add(lightr);
+	objects.add(lightg);
+	objects.add(lightb);
+	objects.add(ball1);
+	objects.add(ball2);
+	objects.add(ball3);
+	objects.add(glass_ball);
+	//objects.add(glass_ball2);
+	return objects;
+}
+
+hittable_list playground() {
+	hittable_list objects;
+
+	auto marble_mat = make_shared<lambertian>(make_shared<marbled_noise_texture>(12));
+	auto ground = make_shared<sphere>(point3(0, -1000, 0), 1000, marble_mat);
+
+	auto glass_mat = make_shared<dialectric>(1.5);
+	auto glass_ball = make_shared<sphere>(point3(0, 1, 0), 1, glass_mat);
+
+	auto light_mat = make_shared<diffuse_light>(color(3, 3, 3));
+	auto light= make_shared<sphere>(point3(-1, 4, -5), 1.0, light_mat);
+	//auto light_pane = make_shared<xy_rect>(1.5, 2.5, 1.5, 2.5, 0, light_mat);
+
+	auto metal_mat = make_shared<metal>(color(0.67, 0.67, 0.67), 0.0);
+	auto steel_ball = make_shared<sphere>(point3(-3, 0.5, 1), 0.5, metal_mat);
+
+	objects.add(ground);
+	objects.add(glass_ball);
+	objects.add(light);
+	objects.add(steel_ball);
+
+	return objects;
 }
 #endif
