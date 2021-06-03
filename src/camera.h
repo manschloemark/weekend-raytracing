01a7e7a7f1be3139a8ea19468b657d55fa302c44
Mark Schloeman <markofwisdumb@gmail.com>
2021-06-03 11:59:10 -0400

    Format camera.h

--- src/camera.h
+++ src/camera.h
@@ -16,26 +16,25 @@ class camera {
 				double _time0 = 0,
 				double _time1 = 0
 				) {
-			//auto aspect_ratio = 16.0 / 9.0;
-      auto theta = degrees_to_radians(vfov);
-      auto h = tan(theta / 2);
-      auto viewport_height = 2.0 * h;
-  		auto viewport_width = aspect_ratio * viewport_height;
-  		auto focal_length = 1.0;
+			auto theta = degrees_to_radians(vfov);
+			auto h = tan(theta / 2);
+			auto viewport_height = 2.0 * h;
+			auto viewport_width = aspect_ratio * viewport_height;
+			//auto focal_length = 1.0;
 
 			w = unit_vector(lookfrom - lookat);
 			u = unit_vector(cross(vup, w));
 			v = cross(w, u);
 
-  		origin = lookfrom;
-  		horizontal = focus_dist * viewport_width * u;
-  		vertical = focus_dist * viewport_height * v;
-  		lower_left_corner = origin - horizontal / 2 - vertical / 2 - focus_dist * w;
+			origin = lookfrom;
+			horizontal = focus_dist * viewport_width * u;
+			vertical = focus_dist * viewport_height * v;
+			lower_left_corner = origin - horizontal / 2 - vertical / 2 - focus_dist * w;
 
-		lens_radius = aperture / 2;
-		time0 = _time0;
-		time1 = _time1;
-	}
+			lens_radius = aperture / 2;
+			time0 = _time0;
+			time1 = _time1;
+		}
 
 		ray get_ray(double s, double t) const {
 			vec3 rd = lens_radius * random_in_unit_disk();
