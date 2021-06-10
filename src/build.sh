d931a80a966353d2b1214c4411f2d06c596b75cc
Mark Schloeman <markofwisdumb@gmail.com>
2021-06-10 09:45:28 -0400

    You can now specify a number of threads in build.sh

--- src/build.sh
+++ src/build.sh
@@ -1,8 +1,16 @@
+# This is pretty sloppy b/c in order to specify the NUM_PROCS you need to also specify the output file... 
+# It would be better if I actually looked at the arguments and identified them.
 if [[ -n $1 ]]
 then filename=$1
-else filename=$(git branch --show-current)
+	if [[ -n $2 ]]
+	then numprocs="-DNUM_THREADS=$2"
+	else numprocs=""
+	fi
+else
+	filename=$(git branch --show-current)
+	numprocs=""
 fi
 
 pushd ../build
-g++ ../src/main.cpp -o $filename -fopenmp
+g++ ../src/main.cpp -o $filename -fopenmp $numprocs
 popd
