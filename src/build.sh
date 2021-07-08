cb958b21d86479c6856e1a7c5bbbd2a04b6a79d8
Mark Schloeman <markofwisdumb@gmail.com>
2021-07-08 15:44:44 -0400

    Updated comment describing build.sh

--- src/build.sh
+++ src/build.sh
@@ -1,5 +1,8 @@
-# This is pretty sloppy b/c in order to specify the NUM_PROCS you need to also specify the output file... 
-# It would be better if I actually looked at the arguments and identified them.
+# Script that makes it easier for me to build my program
+# Just run build.sh FILENAME to compile the program with the appropriate flags
+# Alternatively you can omit the filename and the output file will be named
+# <current_git_branch>.exe
+# Also it uses pushd and popd to build the exe in weekend-raytracing/build/
 if [[ -n $1 ]]
 then filename=$1
 else
